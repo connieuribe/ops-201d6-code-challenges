853b301ac9ed2b73502f266579588b68f2ebfefc
connieuribe <connieuribe95@gmail.com>
2023-02-11 02:56:07 +0000

    Class 05 ops challenge file

---
+++ ops-challenge05.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 04 Ops Challenge Solution
+# Author: Connie Uribe Chavez
+# Date of lates revision: 09 Feb 2023
+# Purpose: Working with arrays and directories
+
+# Main
+
+
+
+
+# End
