914f20c78ca20fb490ee97f967f83cb78a7301b3
connieuribe <connieuribe95@gmail.com>
2023-02-15 04:46:03 +0000

    Created files for class 08,09,10

---
+++ ops-challenge08.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 08 Ops Challenge Solution
+# Author: Connie Uribe Chavez
+# Date of lates revision: 15 Feb 2023
+# Purpose: 
+
+# Main
+
+
+
+
+# End
---
+++ ops-challenge09.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 09 Ops Challenge Solution
+# Author: Connie Uribe Chavez
+# Date of lates revision: 16 Feb 2023
+# Purpose: 
+
+# Main
+
+
+
+
+# End
